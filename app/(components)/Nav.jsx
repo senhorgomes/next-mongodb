b9bf9c31e125022633ff75e9db9789f9edfc0df2
Bryan Gomes <bryanpgomes@gmail.com>
2024-07-23 23:11:37 -0400

    Add icons for navbar and links

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,6 +1,24 @@
+import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Link from "next/link"
+
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+        {/* icons */}
+        <div>
+            <Link href="/">
+                <FontAwesomeIcon icon={faHome} className="icon"/>
+            </Link>
+            <Link href="/TicketPage/new">
+                <FontAwesomeIcon icon={faTicket} className="icon"/>
+            </Link>
+        </div>
+        {/* login */}
+        <div>
+            <p  className="text-default-text">fake@email.com </p>
+        </div>
+    </nav>
   )
 }
 
