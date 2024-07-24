523deeee377b93faedc544c68b9fa58c30ca2152
Bryan Gomes <bryanpgomes@gmail.com>
2024-07-23 22:35:40 -0400

    Import fortawesome

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,10 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "./(components)/Nav";
 
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
