e1f71ccc52f1483018655ef580941e9524b3946a
Bryan Gomes <bryanpgomes@gmail.com>
2024-07-26 17:29:50 -0400

    Completetion of PriorityDisplay

--- app/(components)/PriorityDisplay.jsx
+++ app/(components)/PriorityDisplay.jsx
@@ -1,6 +1,15 @@
+import { faFire } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 const PriorityDisplay = () => {
   return (
-    <div>PriorityDisplay</div>
+    <div className="flex justify-start align-baseline">
+        <FontAwesomeIcon icon={faFire} className="text-red-400"/>
+        <FontAwesomeIcon icon={faFire} className="text-red-400"/>
+        <FontAwesomeIcon icon={faFire} className="text-red-400"/>
+        <FontAwesomeIcon icon={faFire} className="text-red-400"/>
+        <FontAwesomeIcon icon={faFire} className="text-red-400"/>
+    </div>
   )
 }
 
