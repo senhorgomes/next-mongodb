c1ef0b5c9d11014e34e8e09a08db77985a75e6eb
Bryan Gomes <bryanpgomes@gmail.com>
2024-07-27 23:13:49 -0400

    Create MongoDB schema for Ticket

---
+++ app/(models)/Ticket.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+mongoose.connect(process.env.MONDB_URI);
+// Was removed in Mongoose 4???
+// mongoose.Promise = global.Promise;
+// const readyForFlight = async () => {
+//     await mongoose.connect(process.env.MONGODB_URI);
+// }
+
+// readyForFlight().catch((err)=>console.log(err));
+const ticketSchema = Schema(
+    {
+        title: String,
+        description: String,
+        category: String,
+        priority: Number,
+        progress: Number,
+        status: String,
+        active: Boolean,
+    },
+    {
+        timeStamps: true,
+    }
+)
+// Check if ticket exists in database. If it doesn't create it
+const Ticket = mongoose.models.Ticket || mongoose.model("Ticket", ticketSchema);
+export default Ticket;
