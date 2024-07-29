e398951aeacf9ae0fc1b9cc4611c9a5a6913efb8
Bryan Gomes <bryanpgomes@gmail.com>
2024-07-29 00:13:18 -0400

    Added error handling and fetching tickets function

--- app/page.js
+++ app/page.js
@@ -1,6 +1,26 @@
 import TicketCard from "./components/TicketCard";
 
-export default function Dashboard() {
+const getTickets = async () => {
+  try {
+    const res = await fetch("/api/tickets", {cache:"no-store"})
+    return res.json();
+  } catch (error) {
+    console.log(error)
+    return null;
+  }
+}
+
+export default async function Dashboard() {
+  const { tickets } = getTickets();
+
+  if(!tickets){
+    return (
+      <main className="p-5">
+          <h1>Sorry, it appears our services are down. Please try again shortly</h1>
+      </main>
+    )
+  }
+  console.log()
   return (
     <main className="p-5">
       <section className="lg:grid grid-cols-2 xl:grid-cols-4">
