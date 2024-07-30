c2975ce8f07fdd47952c9ff47ca6aa7b59159339
Bryan Gomes <bryanpgomes@gmail.com>
2024-07-30 12:50:42 -0400

    Impletement delete error handling

--- app/components/DeleteBlock.jsx
+++ app/components/DeleteBlock.jsx
@@ -1,15 +1,24 @@
+"use client"
+
 import { faX } from "@fortawesome/free-solid-svg-icons";
 import { useRouter } from "next/navigation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const DeleteBlock = () => {
+const DeleteBlock = ({ id }) => {
 
-  const router = useRouter;
+  const router = useRouter();
 
   const deleteTicket = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/tickets/${id}`, {method: "DELETE"});
-    if(res.ok){
-      router.refresh();
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/tickets/${id}`, {method: "DELETE"});
+
+      if(res.ok){
+        router.refresh();
+      } else {
+        console.error("Failed to delete the ticket");
+      }
+    } catch (error) {
+      console.error("An error occured while deleting the ticket:", error);
     }
   }
 
