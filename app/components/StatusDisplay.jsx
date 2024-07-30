1574cabc5348f60999349346b1ef0882ce386545
Bryan Gomes <bryanpgomes@gmail.com>
2024-07-30 10:56:04 -0400

    Add color for status labels

--- app/components/StatusDisplay.jsx
+++ app/components/StatusDisplay.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
 const StatusDisplay = ({status}) => {
+  
+  // Pending, Started, Completed
+  const getColor = (status) => {
+    if(!status){
+      return "bg-slate-700";
+    }
+    let labelColor = "bg-slate-700"
+    switch (status.toLowerCase()) {
+      case "completed":
+        labelColor = "bg-green-200";
+        return labelColor;
+      case "started":
+        labelColor = "bg-yellow-200";
+        return labelColor;
+      case "pending":
+        labelColor = "bg-red-200";
+        return labelColor;
+    }
+    return labelColor;
+  }
+
   return (
-    <span className='inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 bg-green-200'>
+    <span className={`inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 ${getColor(status)}`}>
         {status}
     </span>
   )
--- app/components/TicketCard.jsx
+++ app/components/TicketCard.jsx
@@ -2,7 +2,7 @@ import DeleteBlock from "./DeleteBlock"
 import PriorityDisplay from "./PriorityDisplay"
 import ProgressDisplay from "./ProgressDisplay"
 import StatusDisplay from "./StatusDisplay"
-const TicketCard = ({title, description, progress, priority, status}) => {
+const TicketCard = ({id, title, description, progress, priority, status}) => {
   return (
     <article className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
       <header className="flex mb-3">
