43b45b48885b909abcd973dfdff988d2d441c386
Bryan Gomes <bryanpgomes@gmail.com>
2024-08-07 19:35:09 -0400

    Wrap userProvider in layout

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "./components/Nav";
-
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
@@ -17,12 +17,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <UserProvider>
         <div className="flex flex-col h-screen max-h-screen">
           <Nav />
           <div className="flex-grow overflow-y-auto bg-page text-default-text">
             {children}
           </div>
         </div>
+        </UserProvider>
       </body>
     </html>
   );
