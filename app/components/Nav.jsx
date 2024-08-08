d67dc9d4d3bedc168008420c34204b4f58223be0
Bryan Gomes <bryanpgomes@gmail.com>
2024-08-07 20:15:55 -0400

    Create login button

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,25 +1,33 @@
-import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useUser } from '@auth0/nextjs-auth0/client';
 import Link from "next/link"
 
 const Nav = () => {
-  return (
-    <nav className="flex justify-between bg-nav p-4">
-        {/* icons */}
-        <div className="flex items-center space-x-4">
-            <Link href="/">
-                <FontAwesomeIcon icon={faHome} className="icon"/>
-            </Link>
-            <Link href="/ticket-page/new">
-                <FontAwesomeIcon icon={faTicket} className="icon"/>
-            </Link>
-        </div>
-        {/* login */}
-        <div>
-            <p  className="text-default-text">fake@email.com </p>
-        </div>
-    </nav>
-  )
+
+    const { user, isLoading } = useUser();
+    return (
+        <nav className="flex justify-between bg-nav p-4">
+            {/* icons */}
+            <div className="flex items-center space-x-4">
+                <Link href="/">
+                    <FontAwesomeIcon icon={faHome} className="icon" />
+                </Link>
+                <Link href="/ticket-page/new">
+                    <FontAwesomeIcon icon={faTicket} className="icon" />
+                </Link>
+            </div>
+            {/* login */}
+            <div>
+                <p className="text-default-text">fake@email.com </p>
+            </div>
+            {!user &&
+                <Link href="#">
+                    <button className="btn">Login</button>
+                </Link>
+            }
+        </nav>
+    )
 }
 
 export default Nav
