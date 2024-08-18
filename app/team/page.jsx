aabaa67b23bcc73d053bb22fb05ca0a4baf1a82a
Bryan Gomes <bryanpgomes@gmail.com>
2024-08-17 23:57:24 -0400

    Set up boilerplate for team page

--- app/team/page.jsx
+++ app/team/page.jsx
@@ -2,7 +2,30 @@ import TicketForm from "@/app/components/TicketForm"
 
 const TeamPage = () => {
     return (
-      <TicketForm/>
+      <div>
+        {/* Table of team members */}
+        <table>
+            <thead>
+                <tr>
+                    <th scope="col">First Name</th>
+                    <th scope="col">Last Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Employee</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scopr="row"></th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+        <form>
+        {/* Form to invite new team member */}
+        </form>
+      </div>
     )
   }
   
